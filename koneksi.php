594a2fd36f17ca9c2946fe0395a3f7b12f65511a
qompac <81723567+qompac@users.noreply.github.com>
2024-01-24 23:16:04 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,13 @@
 <?php 
 
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "baksogresikdb.mysql.database.azure.com";
+$user = "qompac";
+$pass = "Lenovo510";
 $db = "dbpemesanan"; 
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "baksogresikdb.mysql.database.azure.com", "qompac", "{Lenovo510}", "{dbpemesanan}", 3306, MYSQLI_CLIENT_SSL);
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
